e210c45ace872129700883ede6e5ec506f701fbc
DimaTch <4dnik@mail.ru>
2022-02-22 08:04:37 +0200

    rm argument

--- src/telran/validation/Validator.java
+++ src/telran/validation/Validator.java
@@ -20,7 +20,8 @@ public class Validator {
 						res.addAll(validate(field.get(obj)));
 						return;
 					}
-					String message = check(annotation, obj, field);
+					
+					String message = check(annotation, field.get(obj));
 					if (message != null) {
 						res.add(message);
 					}
@@ -32,52 +33,49 @@ public class Validator {
 		return res;
 	}
 
-	private static String check(Annotation annotation, Object object, Field field) throws Exception {
+	private static String check(Annotation annotation, Object object) throws Exception {
 		String nameMethod = "valid" + annotation.annotationType().getSimpleName();
-		Method method = Validator.class.getDeclaredMethod(nameMethod, Annotation.class, Object.class, Field.class);
-		return (String) method.invoke(Validator.class, annotation, object, field);
+		Method method = Validator.class.getDeclaredMethod(nameMethod, Annotation.class, Object.class);
+		return (String) method.invoke(Validator.class, annotation, object);
 	}
 
 	@SuppressWarnings("unused")
-	private static String validNotNull(Annotation annotation, Object object, Field field) throws Exception {
-		Object value = field.get(object);
-		return value == null ? getMessage(annotation) : null;
+	private static String validNotNull(Annotation annotation, Object object) throws Exception {
+		return object == null ? getMessage(annotation) : null;
 	}
 
 	@SuppressWarnings("unused")
-	private static String validNotEmpty(Annotation annotation, Object object, Field field) throws Exception {
-		String value = (String) field.get(object);
-		return value.length() == 0 ? getMessage(annotation) : null;
+	private static String validNotEmpty(Annotation annotation, Object object) throws Exception {
+		return ((String) object).length() == 0 ? getMessage(annotation) : null;
 	}
 
 	@SuppressWarnings("unused")
-	private static String validMin(Annotation annotation, Object object, Field field) throws Exception {
-		Object value = field.get(object);
-		return value.getClass().getSimpleName().equals("Integer")
-				? (int) value < getValue(annotation) ? getMessage(annotation) : null
-				: (long) value < getValue(annotation) ? getMessage(annotation) : null;
+	private static String validMin(Annotation annotation, Object object) throws Exception {
+		return object.getClass().getSimpleName().equals("Integer")
+				? (int) object < getValue(annotation) ? getMessage(annotation) : null
+				: (long) object < getValue(annotation) ? getMessage(annotation) : null;
 
 	}
 
 	@SuppressWarnings("unused")
-	private static String validMax(Annotation annotation, Object object, Field field) throws Exception {
-		return (long) field.get(object) > getValue(annotation) ? getMessage(annotation) : null;
+	private static String validMax(Annotation annotation, Object object) throws Exception {
+		return (long) object > getValue(annotation) ? getMessage(annotation) : null;
 	}
 
 	@SuppressWarnings("unused")
-	private static String validPast(Annotation annotation, Object object, Field field) throws Exception {
-		return LocalDate.now().isBefore((LocalDate) field.get(object)) ? getMessage(annotation) : null;
+	private static String validPast(Annotation annotation, Object object) throws Exception {
+		return LocalDate.now().isBefore((LocalDate) object) ? getMessage(annotation) : null;
 	}
 
 	@SuppressWarnings("unused")
-	private static String validFuture(Annotation annotation, Object object, Field field) throws Exception {
-		return LocalDate.now().isAfter((LocalDate) field.get(object)) ? getMessage(annotation) : null;
+	private static String validFuture(Annotation annotation, Object object) throws Exception {
+		return LocalDate.now().isAfter((LocalDate) object) ? getMessage(annotation) : null;
 	}
 
 	@SuppressWarnings("unused")
-	private static String validEmail(Annotation annotation, Object object, Field field) throws Exception {
+	private static String validEmail(Annotation annotation, Object object) throws Exception {
 		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
-		return ((String) field.get(object)).matches(regex) ? "" : getMessage(annotation);
+		return ((String) object).matches(regex) ? "" : getMessage(annotation);
 	}
 
 	private static String getMessage(Annotation annotation) throws Exception {
